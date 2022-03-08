35dc068ac2ef77753817323e9de451a9a0ce45d4
Lanil Marasinghe <lanil@bitbucket.org>
2022-03-08 19:56:50 +0530

    error handling

--- routes/photos.js
+++ routes/photos.js
@@ -4,8 +4,13 @@ import { photos } from '../api';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const photosResponse = await photos.getPhotos();
-  res.json(photosResponse.data);
+  try {
+    const photosResponse = await photos.getPhotos();
+    res.json(photosResponse.data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ status: 500, message: e.toString() });
+  }
 });
 
 export default router;
--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,15 @@ import { formatUsersResponse } from '../utils/usersUtils';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const usersResponse = await users.getUsers();
-  console.log('user response received');
-  console.log(usersResponse.data);
-  res.json(formatUsersResponse(usersResponse.data));
+  try {
+    const usersResponse = await users.getUsers();
+    console.log('user response received');
+    console.log(usersResponse.data);
+    res.json(formatUsersResponse(usersResponse.data));
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ status: 500, message: e.toString() });
+  }
 });
 
 export default router;
