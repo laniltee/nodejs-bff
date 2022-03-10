f1fb473797f1e7ba9827464a0169cfaa6751b5ab
Lanil Marasinghe <lanil@bitbucket.org>
2022-03-10 17:51:24 +0530

    prettier eslint check

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 import users from './users';
 import photos from './photos';
+import todos from './todos';
 
 router.use('/users', users);
 router.use('/photos', photos);
+router.use('/todos', todos);
 
 export default router;
